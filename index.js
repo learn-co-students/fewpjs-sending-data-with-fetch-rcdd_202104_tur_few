1b5c048eb55ca5cb70e5aebf0c0082efca890425
Hafise Nur Bacaksız <hafisenur.bacaksiz@gmail.com>
2021-05-02 16:17:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name,email) {
+
+
+let formData = {
+    name,
+    email,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(json => (document.body.innerHTML = json.id))
+    .catch(error => (document.body.innerHTML = error.message));
+}
