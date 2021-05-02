65d9cdf365f152594892cb983f2736be20871518
Rahaf Shora <rahaf.it@gmail.com>
2021-05-02 03:10:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email) {
+  const formData = {
+    name,
+    email,
+  };
+
+  // method: "POST" is missing from the object below
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(json => (document.body.innerHTML = json.id))
+    .catch(error => (document.body.innerHTML = error.message));
+}
