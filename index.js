2c3009df59ba970b29460041ec8a3a1a996c6df7
iseluin <kutay.kagan.ozenn@gmail.com>
2021-05-14 14:03:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+
+    const destinationURL = "http://localhost:3000/users";
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({
+        name: `${userName}`,
+        email: `${userEmail}`
+        })
+    };
+
+    return fetch(destinationURL, configurationObject)
+           .then(response => response.json())
+           .then(json => (document.body.innerHTML = json['id']))
+           .catch(err => (document.body.innerHTML = err.message))
+}
